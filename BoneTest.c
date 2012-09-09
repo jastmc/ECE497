3571f80d54c1f97a4e09e499a1f518c54d7c66a1
millerap <millerap@rose-hulman.edu>
2012-09-09 17:56:14 -0400

    test code for set_gpio_direction and set_gpio value added for Beagle. Still does not fully set value yet

--- BoneTest.c
+++ BoneTest.c
@@ -5,6 +5,10 @@
 // int set_gpio_direction(int gpio, char* direction);
 // int set_gpio_value(int gpio, int value);
 int main(int argc, char** argv){
-	int export_gpio(60);
+	export_gpio(60);
+	set_gpio_direction(60, "out");
+	set_gpio_value(60, 1);
+	set_gpio_direction(60, "in");
+	set_gpio_value(60, 1);
 	return 0;
 }
